37ec6b35a7baa8294851343611fd6a18cfbe59ec
jkwe9746 <jkwe7777@qq.com>
2020-07-13 12:40:52 +0800

    I am initial

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yznw7777
+ * @create 2020-07-13 12:36
+ */
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("I love you");
+    }
+}
