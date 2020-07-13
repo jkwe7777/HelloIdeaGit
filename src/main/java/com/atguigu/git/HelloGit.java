1620ba8aa7bbb483f060c64131dea9e4f4a50ced
jkwe9746 <jkwe7777@qq.com>
2020-07-13 13:08:10 +0800

    I am new branch A

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -8,5 +8,7 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("I love you");
         System.out.println("I love you");
+        System.out.println("I am new branch A");
+
     }
 }
