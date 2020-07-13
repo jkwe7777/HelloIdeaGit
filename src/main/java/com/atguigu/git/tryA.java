fa177ea638152252763deeec93f6858f8d9b5614
jkwe9746 <jkwe7777@qq.com>
2020-07-13 14:49:06 +0800

    I am change

---
+++ src/main/java/com/atguigu/git/tryA.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yznw7777
+ * @create 2020-07-13 14:48
+ */
+public class tryA {
+    public static void main(String[] args){
+        System.out.println("What do you main?");
+    }
+}
